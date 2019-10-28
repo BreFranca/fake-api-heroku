70dc65e467607a7df8ce77960ce980c9bc29a4b5
BreFranca <bre.sfranca@gmail.com>
2019-10-28 13:33:37 -0300

    Update port

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 mongoose = require('mongoose'),
 
-mongoose.connect('mongodb://localhost:27017/annotationdb', {
+mongoose.connect('mongodb://localhost/annotationdb', {
+    useUnifiedTopology: true,
     useNewUrlParser: true
 })
