2cb991adaea1204a66db75fe3a3930978cfe0fb6
BreFranca <bre.sfranca@gmail.com>
2019-10-28 14:06:32 -0300

    Update cors

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const server = express()
 const port = process.env.PORT || 4000
+const cors = require('cors');
 const bodyParser = require('body-parser')
+
+server.use(cors())
 
 server.use(bodyParser.urlencoded({ extended: true }))
 
